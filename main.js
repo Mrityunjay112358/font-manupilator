7992a1c7058e9e3b88da55f70f7a93815e2bd50d
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2021-05-14 10:26:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+nosex=0;
+nosey=0;
+difference=0;
+rightwristx=0;
+leftwristx=0;
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.position(680,70)
+    video = createCapture(VIDEO);
+    video.size(600,500);
+}
