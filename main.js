597365338f362083d5172d6878d0c51b535670ae
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2021-05-15 19:10:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-nosex=0;
-nosey=0;
+rightwristy=0;
+leftwristy=0;
 difference=0;
 rightwristx=0;
 leftwristx=0;
@@ -9,4 +9,22 @@ function setup(){
     canvas.position(680,70)
     video = createCapture(VIDEO);
     video.size(600,500);
+posenet = ml5.poseNet(video,modelLoaded);
+posenet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+console.log("model is initialized");
+}
+
+function gotPoses(results){
+if(results.length>0){
+console.log(results);
+leftwristx = results[0].pose.leftWrist.x;
+rightwristx = results[0].pose.rightWrist.x;
+leftwristy = results[0].pose.leftWrist.y;
+rightwristy = results[0].pose.rightWrist.y;
+
+console.log("leftwristx = "+leftwristx+" leftwristY = "+leftwristy+" rightwristx = "+rightwristx+" rightwristy = "+rightwristy);
+}
 }
